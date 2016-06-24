ba08e4225d79ae4ffd8123aced7e0acd7d91fa5c
Manuel Maly <manuel.maly@gmail.com>
2016-06-24 14:14:22 +0200

    Removing Example/Pods from git.

--- Example/Pods/Target Support Files/keyper-ios-sdk/keyper-ios-sdk-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_keyper_ios_sdk : NSObject
-@end
-@implementation PodsDummy_keyper_ios_sdk
-@end
--- Example/Pods/Target Support Files/keyper-ios-sdk/keyper-ios-sdk-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double keyper_ios_sdkVersionNumber;
-FOUNDATION_EXPORT const unsigned char keyper_ios_sdkVersionString[];
-
